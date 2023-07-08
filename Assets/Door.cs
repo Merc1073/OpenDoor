901b5b2dcfed947bf9a980eb2b5c8ff99a40dd5a
Merc1073 <sergeikoblov2000@gmail.com>
2023-07-08 20:28:22 +0100

    It's done

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Player player;
+
     void Update()
     {
-        
+        if(player.GetComponent<Player>().Coins == 5)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
